f4afa8735780ebdbc9d18649a31f7f9891916500
Josh Millard <josh.millard10@gmail.com>
2021-11-11 13:44:46 -0500

    feature/admin-categories implimented

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -13,7 +13,7 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to [:admin, :category], notice: 'Category created!'
+      redirect_to [:admin, :categories], notice: 'Category created!'
     else
       render :new
     end
@@ -23,5 +23,14 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.find params[:id]
     @category.destroy
     redirect_to [:admin, :categories], notice: 'Category deleted!'
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+      
+    )
   end
 end
